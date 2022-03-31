82edbcd085e6abde49de872b36be5425965975f7
Douglas Bonomo <dwbono01@gmail.com>
2022-03-31 18:41:33 -0400

    basic layout, navmenu, home component migrated

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Routes } from 'react-router';
+import { Home } from './components/Home';
+import { Layout } from './components/Layout';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-                  Edit <code>src/App.tsx</code> and save to reload.
-                  Wonderful wonderful html.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    return (
+        <Layout>
+            <Routes>
+                <Route path='/' element={<Home />} />
+            </Routes>
+        </Layout>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const baseUrl : string = document.getElementsByTagName('base')[0]?.getAttribute('href') ?? '';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+    <React.StrictMode>
+        <BrowserRouter basename={baseUrl}>
+            <App />
+        </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
