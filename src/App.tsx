9b1f72ee2f46feecb57da8a5925c28f0c2762023
Douglas Bonomo <dwbono01@gmail.com>
2022-04-01 17:01:19 -0400

    add counter app to routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { Layout } from './components/Layout';
 import logo from './logo.svg';
 import './App.css';
 import { FetchData } from './components/FetchData';
+import { Counter } from './components/Counter';
 
 function App() {
     return (
         <Layout>
             <Routes>
                 <Route path='/' element={<Home />} />
+                <Route path='/counter' element={<Counter />} />
                 <Route path='/fetch-data' element={<FetchData />} />
             </Routes>
         </Layout>
